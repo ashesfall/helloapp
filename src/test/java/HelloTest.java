4e2e8349126583e2303d18f30439b910fcfdf806
ashesfall@almostrealism.com <ashesfall@almostrealism.com>
2018-05-17 10:56:52 -0700

    Test

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,2 +1,9 @@
-public class HelloTest {
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class HelloTest extends TestCase {
+	@Test
+	public void checkMessage() {
+		assertEquals(new HelloService().doHello(), "Hello, World");
+	}
 }
